bad7eb151342d5b27839a2c8caaefc01d9ea1277
anekdotchik <32750962+anekdotchik@users.noreply.github.com>
2021-05-15 22:10:39 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 let degree = 0
 basic.forever(function () {
     degree = input.compassHeading()
-    if (degree < 45) {
-        basic.showString("N")
-    } else if (degree < 135) {
-        basic.showString("E")
-    } else if (degree < 255) {
-        basic.showString("S")
-    } else if (degree < 315) {
-        basic.showString("W")
+    if (degree < 23) {
+        images.arrowImage(ArrowNames.North).showImage(0)
+    } else if (degree < 68) {
+        images.arrowImage(ArrowNames.NorthWest).showImage(0)
+    } else if (degree < 113) {
+        images.arrowImage(ArrowNames.West).showImage(0)
+    } else if (degree < 158) {
+        images.arrowImage(ArrowNames.SouthWest).showImage(0)
+    } else if (degree < 203) {
+        images.arrowImage(ArrowNames.South).showImage(0)
+    } else if (degree < 248) {
+        images.arrowImage(ArrowNames.SouthEast).showImage(0)
+    } else if (degree < 293) {
+        images.arrowImage(ArrowNames.East).showImage(0)
+    } else if (degree < 338) {
+        images.arrowImage(ArrowNames.NorthEast).showImage(0)
     } else {
-        basic.showString("N")
+        images.arrowImage(ArrowNames.North).showImage(0)
     }
 })
