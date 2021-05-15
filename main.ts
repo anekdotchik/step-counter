0c4dfca0fb237257f4433973e4d0024a4f62d40a
anekdotchik <32750962+anekdotchik@users.noreply.github.com>
2021-05-15 21:37:46 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-input.onGesture(Gesture.Shake, function () {
-    step += 1
-    led.stopAnimation()
-})
-let step = 0
+let degree = 0
 basic.forever(function () {
-    basic.showNumber(step)
+    degree = input.compassHeading()
+    if (degree < 45) {
+        basic.showString("N")
+    } else if (degree < 135) {
+        basic.showString("E")
+    } else if (degree < 255) {
+        basic.showString("S")
+    } else if (degree < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
 })
