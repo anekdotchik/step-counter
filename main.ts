e5b8d4001a463b70514a609e16c7b3723a1f66bb
anekdotchik <32750962+anekdotchik@users.noreply.github.com>
2021-05-27 22:36:27 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,19 +4,19 @@ basic.forever(function () {
     if (degree < 23) {
         images.arrowImage(ArrowNames.North).showImage(0)
     } else if (degree < 68) {
-        images.arrowImage(ArrowNames.NorthWest).showImage(0)
+        images.arrowImage(ArrowNames.NorthEast).showImage(0)
     } else if (degree < 113) {
-        images.arrowImage(ArrowNames.West).showImage(0)
+        images.arrowImage(ArrowNames.East).showImage(0)
     } else if (degree < 158) {
-        images.arrowImage(ArrowNames.SouthWest).showImage(0)
+        images.arrowImage(ArrowNames.SouthEast).showImage(0)
     } else if (degree < 203) {
         images.arrowImage(ArrowNames.South).showImage(0)
     } else if (degree < 248) {
-        images.arrowImage(ArrowNames.SouthEast).showImage(0)
+        images.arrowImage(ArrowNames.SouthWest).showImage(0)
     } else if (degree < 293) {
-        images.arrowImage(ArrowNames.East).showImage(0)
+        images.arrowImage(ArrowNames.West).showImage(0)
     } else if (degree < 338) {
-        images.arrowImage(ArrowNames.NorthEast).showImage(0)
+        images.arrowImage(ArrowNames.NorthWest).showImage(0)
     } else {
         images.arrowImage(ArrowNames.North).showImage(0)
     }
